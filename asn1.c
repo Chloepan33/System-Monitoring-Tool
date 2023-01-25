7f9d94bcab5ab8f21a18d9e2f922430d54f2a939
Chloepan33 <zypan03@gmail.com>
2023-01-25 13:40:57 -0500

    Update asn1.c

--- asn1.c
+++ asn1.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 #include <sys/utsname.h>
+#include <sys/resource.h>
+#include <sys/sysinfo.h>
+
 
 void SystemInfo(){
    struct utsname uts;
-   if (uname(&uts) < 0)
-    perror("uname() error");
-    else {
+   if (uname(&uts) < 0){
+      perror("SystemInfo error");
+   }   
+   else {
       printf("###System Information");
       printf("System Name:  %s\n", uts.sysname);
-      printf("Machine Name:  %s\n", uts.machine);
+      printf("Machine Name:  %s\n", uts.nodename);
       printf("Version:  %s\n", uts.version);
       printf("Release:  %s\n", uts.release);
-      // what is arcitecture 
-      //  printf("Nodename: %s\n", uts.nodename);
+      printf("Architecture: %s\n", uts.machine);
     }
+
+}
+
+void MemoryUsage(){
+   struct rusage r_usage;
+   if (getrusage(RUSAGE_SELF,&r_usage)<0){
+      perror("MemoryUsage error");
+   }
+   else{
+      printf("Memory usage: %ld kilobytes\n",r_usage.ru_maxrss);
+   }
+
+}
+
+void Memory(){
+   struct sysinfo info;
+   if(sysinfo(&info)<0){
+      perrorz("Memory error");
+   }
+   else{
+      printf
+   }
 
 }
 
 int main() {
    SystemInfo();
+   MemoryUsage();
 }
