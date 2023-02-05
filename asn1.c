5ba94363fb5b98576ed4f3bcd35bf45d3f0da541
Chloepan33 <zypan03@gmail.com>
2023-02-04 19:33:07 -0500

    sb

--- asn1.c
+++ asn1.c
@@ -82,21 +82,14 @@ void ShowMemoryUsage()
 }
 
 /**
- * @brief Displaying memory information, in unit of GB, including
- *    total physical memory,
- *    used physical memory,
- *    total virtual memory,
- *    used virtual memory.
- *
- *    Memories are calculated by reading file /proc/meminfo.
- *    total physical memory = totalram
- *    used physical memory = (totalram - freeram)  - (bufferram + cachedram + SReclaimable)
- *    total virtual memory = totalram + total swap
- *    used virtual memory = used physical memory + totalswap - freeswap
- *
- * @return the used physical memory
- *
+ * @brief Displaying memory variation represented by graph
+ * 
+ * @param pre previous memory size
+ * @param post current memory size
+ * 
+ * @return void
  */
+
 void MemroyGraph(double pre, double post)
 {
 
@@ -133,9 +126,27 @@ void MemroyGraph(double pre, double post)
       }
       diff = -diff;
    }
-   printf(" %.2f (%.2f)\n",diff,post);
+   printf(" %.2f (%.2f)\n", diff, post);
 }
 
+/**
+ * @brief Displaying memory information, in unit of GB, including
+ *    total physical memory,
+ *    used physical memory,
+ *    total virtual memory,
+ *    used virtual memory.
+ *
+ *    Memories are calculated by reading file /proc/meminfo.
+ *    total physical memory = totalram
+ *    used physical memory = (totalram - freeram)  - (bufferram + cachedram + SReclaimable)
+ *    total virtual memory = totalram + total swap
+ *    used virtual memory = used physical memory + totalswap - freeswap
+ *
+ * @param pre value of previous used memory size
+ * @param graph_state to indicate whether or not to show graphics
+ * @return the used physical memory
+ *
+ */
 double ShowMemory(double pre, int graph_state)
 {
    FILE *meminfo = fopen("/proc/meminfo", "r");
@@ -180,11 +191,13 @@ double ShowMemory(double pre, int graph_state)
    total_virtual = totalram + totalswap;
    virtual_used = phys_used + totalswap - freeswap;
    printf("%.2f GB / %.2f GB  -- %.2f GB / %.2f GB", phys_used * 1e-9, total_phys * 1e-9, virtual_used * 1e-9, total_virtual * 1e-9);
-   if(graph_state == 0){
+   if (graph_state == 0)
+   {
       printf("\n");
    }
-   else{
-      MemroyGraph(pre*1e-9, phys_used*1e-9);
+   else
+   {
+      MemroyGraph(pre * 1e-9, phys_used * 1e-9);
    }
    return (double)phys_used;
 }
@@ -229,6 +242,7 @@ void ShowCore()
  * @brief Displaying the utilization percentage of CPU by reading file /proc/stat
  *
  * @param period indicate how frequently to sample in seconds
+ * 
  * @return the utilization percentage of CPU
  */
 double ShowCpu(int period)
@@ -258,39 +272,12 @@ double ShowCpu(int period)
 }
 
 /**
- * @brief Displaying system information including memory usage and cpu usage
- *
- * @param sample_size indicate how many times the statistics are going to be collected
- * @param period indicate how frequently to sample in seconds
- * @return void
- */
-void ShowSystem(int sample_size, int period)
-{
-
-   printf("----------------------------\n");
-   printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot) \n");
-   for (int i = 0; i < sample_size; i++)
-   {
-      printf("\n");
-   }
-   ShowCore();
-   moveUp(sample_size + 2);
-   for (int m = sample_size + 1; m > 1; m--)
-   {
-      double mem = ShowMemory(0,0);
-      moveDown(m);
-      double cpu = ShowCpu(period);
-      moveUp(m + 1);
-   }
-   moveDown(3);
-}
-
-/**
  * @brief Displaying cpu usage in graphic form
  *
  *    The number of symbol "|" is equals to number of percentage
  *
  * @param cpu used cpu percentage
+ * 
  * @return void
  */
 void CpuGraph(double cpu)
@@ -304,14 +291,17 @@ void CpuGraph(double cpu)
 }
 
 /**
- * @brief Displaying system information including memory usage and cpu usage in graphic form
+ * @brief Displaying system information including memory usage and cpu usage
  *
  * @param sample_size indicate how many times the statistics are going to be collected
  * @param period indicate how frequently to sample in seconds
+ * @param graphic_state indicate whether or not to show graphics
+ * 
  * @return void
  */
-void ShowSystemGraph(int sample_size, int period)
+void ShowSystem(int sample_size, int period, int graphic_state)
 {
+
    printf("----------------------------\n");
    printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot) \n");
    for (int i = 0; i < sample_size; i++)
@@ -323,34 +313,54 @@ void ShowSystemGraph(int sample_size, int period)
    double pre = -1;
    for (int m = sample_size + 1; m > 1; m--)
    {
-      pre = ShowMemory(pre,1);
+      pre = ShowMemory(pre, graphic_state);
       moveDown(m);
       double cpu = ShowCpu(period);
-      moveDown(sample_size + 1 - m);
-      CpuGraph(cpu);
-      moveUp(sample_size + 3);
+      if (graphic_state == 0)
+      {
+         moveUp(m + 1);
+      }
+      else if (graphic_state == 1)
+      {
+         moveDown(sample_size + 1 - m);
+         CpuGraph(cpu);
+         moveUp(sample_size + 3);
+      }
    }
-   moveDown(sample_size + 3);
+   moveDown(sample_size * graphic_state + 3);
 }
+
 
 /**
  * @brief Displaying system information including memory usage and cpu usage in sequantial form
  *
  * @param sample_size indicate how many times the statistics are going to be collected
  * @param period indicate how frequently to sample in seconds
+ * @param graphic_state indicate whether or not to show graphics
+ * 
  * @return void
  */
-void ShowSequentials(int sample_size, int period,int graph_state)
+void ShowSequentials(int sample_size, int period, int graph_state)
 {
-   double pre = 0;
+   double pre = -1;
    for (int i = 0; i < sample_size; i++)
    {
       printf(">>> iteration %d\n", i + 1);
       ShowMemoryUsage();
       printf("----------------------------\n");
       printf("### Memory ### (Phys.Used/Tot -- Virtual Used/Tot) \n");
-      pre = ShowMemory(pre,graph_state);
+      for (int m = 0; m < i; m++){
+         printf("\n");
+      }
+      pre = ShowMemory(pre, graph_state);
+       for (int c = 1; c < sample_size - i; c++){
+         printf("\n");
+      }
+      printf("----------------------------\n");
       double cpu = ShowCpu(period);
+      if(graph_state == 1){
+         CpuGraph(cpu);
+      }
       printf("----------------------------\n");
    }
 }
@@ -360,6 +370,9 @@ int main(int argc, char *argv[])
    int sample_size = 10;
    int period = 1;
 
+   int count_int = 0;
+   int tem_int = 0;
+
    int system_state = 0;
    int user_state = 0;
    int graphic_state = 0;
@@ -367,11 +380,10 @@ int main(int argc, char *argv[])
 
    if (argc == 1)
    {
-      // printf("Nbr of samples: %d -- every %d secs\n", sample_size, period);
-      // ShowSystem(sample_size, period);
-      // ShowUser();
-      // ShowSystemInfo();
-      ShowSystemGraph(sample_size,period);
+      printf("Nbr of samples: %d -- every %d secs\n", sample_size, period);
+      ShowSystem(sample_size, period, graphic_state);
+      ShowUser();
+      ShowSystemInfo();
    }
    else
    {
@@ -389,7 +401,7 @@ int main(int argc, char *argv[])
          {
             graphic_state = 1;
          }
-         else if (strcmp(argv[i], "--sequentials") == 0)
+         else if (strcmp(argv[i], "--sequential") == 0)
          {
             sequential_state = 1;
          }
@@ -401,6 +413,22 @@ int main(int argc, char *argv[])
          {
             printf("The current sample frequency is %d sec\n", period);
          }
+         else if(sscanf(argv[i], "%d", &tem_int) == 1 && (tem_int > 0)){
+            if (count_int == 2){
+               printf("To many input integers!\n");
+               exit(0);
+            }
+            else if (count_int == 1){
+               period = tem_int;
+               tem_int = 0;
+               count_int = 2;
+            }
+            else if(count_int == 0){
+               sample_size = tem_int;
+               tem_int = 0;
+               count_int = 1;
+            }
+         }
          else
          {
             printf("Invalid command line arguments\n");
@@ -408,13 +436,37 @@ int main(int argc, char *argv[])
          }
       }
       printf("Nbr of samples: %d -- every %d secs\n", sample_size, period);
-      if (user_state == 1 && system_state == 1)
+      if (user_state == 1)
       {
-         printf("command combinition invalid\n");
-         exit(0);
+         if (system_state == 1 || sequential_state == 1 || graphic_state == 1)
+         {
+            printf("command combinition invalid\n");
+            exit(0);
+         }
+         else
+         {
+            ShowUser();
+         }
       }
-      else if (user_state == 1)
+      else if (system_state == 1)
       {
+         if (sequential_state == 1)
+         {
+            ShowSequentials(sample_size, period, graphic_state);
+         }
+         else
+         {
+            ShowSystem(sample_size, period, graphic_state);
+         }
+      }
+      else if (sequential_state == 1)
+      {
+         ShowSequentials(sample_size, period, graphic_state);
+         ShowUser();
+      }
+      else
+      {
+         ShowSystem(sample_size, period, graphic_state);
          ShowUser();
       }
       ShowSystemInfo();
